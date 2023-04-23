cd7c38b5f4107ca1aa2b5a5d1795ed1c1c100306
EF-Pami <119734583+EF-Pami@users.noreply.github.com>
2023-04-23 22:31:30 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 
 async function movies() {
+    const moviecontainer = document.querySelector (".moviecontainer");
     const url ="https://api.noroff.dev/api/v1/square-eyes";
     const response = await fetch(url);
     const result = await response.json();
+
+    moviecontainer.innerHTML = (result);
     return result;
+
     
     
 }
@@ -16,6 +20,8 @@ const url = "https://api.noroff.dev/api/v1/square-eyes/b9e4edb1-e798-45e3-9c46-f
 async function fetchmovie(){
     const response = await fetch(url);
     const details = await response.json();
+
+    product.innerHTML =(details);
     return details;
     
 }
